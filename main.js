b36e541a464cd6f1bdaeff5ca3b138df4335f081
gtorres11 <68196892+gtorres11@users.noreply.github.com>
2020-07-20 21:40:04 -0300

    git

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.banner1', { delay: 50});
+ScrollReveal().reveal('.banner2', { delay: 1000 });
+ScrollReveal().reveal('.banner3pre', { delay: 1000 });
+ScrollReveal().reveal('.banner3', { delay: 1000 });
+ScrollReveal().reveal('.banner4', { delay: 1000 });
