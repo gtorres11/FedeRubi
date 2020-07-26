ed4fecdec17da8efd635e137bba248dd7350e2a0
gtorres11 <68196892+gtorres11@users.noreply.github.com>
2020-07-26 20:29:18 -0300

    ultimos cambios

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
 ScrollReveal().reveal('.banner1', { delay: 50});
 ScrollReveal().reveal('.banner2', { delay: 1000 });
 ScrollReveal().reveal('.banner3pre', { delay: 1000 });
 ScrollReveal().reveal('.banner3', { delay: 1000 });
 ScrollReveal().reveal('.banner4', { delay: 1000 });
+
